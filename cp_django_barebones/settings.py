69a2c9a0f349fda149eb0d5a85db88ac6bde5172
Tonysiu <thefool363@gmail.com>
2019-09-13 19:09:52 +0800

    changes

--- cp_django_barebones/settings.py
+++ cp_django_barebones/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,10 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+    
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configure Django App for Heroku.
-
+import django_heroku
 django_heroku.settings(locals())
